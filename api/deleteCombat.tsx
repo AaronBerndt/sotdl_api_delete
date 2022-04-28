ca772931e94a64cffd2b397b618381c19f7051f5
Aaron <aberndt@nagios.com>
2022-04-28 18:09:47 -0500

    Add deleteCombat api

---
+++ api/deleteCombat.tsx
@@ -0,0 +1,26 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { deleteFromCollection } from "../utilities/MongoUtils";
+import { ObjectId } from "mongodb";
+import microCors from "micro-cors";
+
+const cors = microCors();
+
+const handler = async (request: VercelRequest, response: VercelResponse) => {
+  try {
+    if (request.method === "OPTIONS") {
+      return response.status(200).end();
+    }
+
+    const id: any = request.query._id;
+
+    const data = await deleteFromCollection("combats", {
+      _id: new ObjectId(id),
+    });
+    response.status(200).send(data);
+  } catch (e) {
+    console.log(e);
+    response.status(504).send(e);
+  }
+};
+
+export default cors(handler);
